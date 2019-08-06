2bace0fb2045fb3e0ae952a7cb67d7e6c21c1d2a
Myles Carey <seanmyles@comcast.net>
2019-08-06 16:41:42 -0700

    player signin ready for testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -82,12 +82,12 @@ $(document).ready(function(){
     projectId: "rockpaperscissorlizardsp-393e9",
     storageBucket: "",
     messagingSenderId: "157845538465",
-    appId: "1:157845538465:web:1323a3fecbac83cb"};
+    appId: "1:157845538465:web:1323a3fecbac83cb"}
 
-  // initialize firebase
+  // // initialize firebase
   firebase.initializeApp(firebaseConfig);
 
-  // variable to firebase database
+  // // variable to firebase database
   var database = firebase.database();
 
   // connectionsRef references a specific location in our database.
@@ -98,6 +98,8 @@ $(document).ready(function(){
   // the client's connection state changes.
   // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
   var connectedRef = database.ref(".info/connected");
+
+
 
   
 
@@ -110,10 +112,11 @@ $(document).ready(function(){
   // objects and classes
   // ---------------------------------------------------------
 
-  // game objedc t
+  // game object
   var game = {
     // local variables
     gameState: "not started",
+    youArePlayer: 0,
     playerName1: "",
     playerName2: "",
     playerChoice1: "",
@@ -134,12 +137,93 @@ $(document).ready(function(){
   //  START OF PROGRAM FLOW
   // ----------------------------------------------------------------------------
 
+   console.log("Starting the program");
+
    
   // ----------------------------------------------------------------------------
   // Events and timers
   // ----------------------------------------------------------------------------
  
+  // get value from name box
+  $("#set-name-btn").on("click", function(event) {
+    console.log("in global.set-name-btn click event");
+    event.preventDefault();
+    var name = $("#name-input").val().trim();
+    console.log("right now i am player: ", game.youArePlayer);
+ 
+    if (game.youArePlayer === 0) {
+      game.youArePlayer = 1;
+      game.playerName1 = name;
+      database.ref("/game").set({
+        playerName1: game.playerName1,
+        playerName2: ""
+      });
+      $("#left-player").text(name);
+    }
+    else {  // you have to be player two by now
+      // game.youArePlayer = 2;
+      game.playerName2 = name;
+      database.ref("/game").set({
+        playerName1: game.playerName1,
+        playerName2: game.playerName2
+      });
+      $("#right-player").text(name);
+    };
+
+    console.log("right now i am player: ", game.youArePlayer);
+
+    // // player 1 on the left player 2 on the rigth
+    // if (game.youArePlayer === 1) {
   
+    // }
+    // else {
+    //   $("#right-player").text(name);
+    // };
+   
+
+    // hide the name entry 
+    $("#name-input").hide();
+    $("#set-name-btn").hide();
+
+  });
+
+
+    
+  
+
+
+  // get firebase snapshot on initial load and when database changes
+  database.ref("/game").on("value", function (snap) {
+    console.log("in global.database ref /players value event");
+    console.log(snap.val());
+    
+    // if no player yet then you are firebase player 1 else firebase player 2
+    if (snap.child("playerName1").exists()) {
+
+      if (game.youArePlayer === 1) { // first to enter a name 
+        console.log("this is player 1")
+        $("#right-player").text(snap.val().playerName2);
+      }
+      else {  // player 0 becoming player 1
+        console.log("this is player 2")
+        game.youArePlayer = 2;
+        $("#left-player").text(snap.val().playerName1);
+      };
+
+      // console.log("if you haven't entered name yet then you will be player 2");
+      // // you are player two now, but still have to enter you name
+      // if (game.youArePlayer === 0) {
+      //   game.youArePlayer = 2;
+      // };
+
+      game.playerName1 = snap.val().playerName1;
+      };
+
+
+
+  });
+
+
 
   // When the client's connection state changes...
   connectedRef.on("value", function(snap) {
@@ -153,7 +237,9 @@ $(document).ready(function(){
     }
   });
 
-// When first loaded or when the connections list changes...
+
+
+  // When first loaded or when the connections list changes...
   connectionsRef.on("value", function(snapshot) {
 
     // Display the viewer count in the html.
@@ -165,6 +251,9 @@ $(document).ready(function(){
   // instruction button event - show modal
   $("#instruction-btn").on("click",function() {
     console.log("in global.insturction-btn click event")
+   // clear the variable
+   var firebaseGame = database.ref("/game");
+   firebaseGame.remove();
    // show insructions
     $('#my-modal').modal('show');
   });
@@ -172,11 +261,10 @@ $(document).ready(function(){
   
   // choice click
   $(".list-group-item-dark").on("click", function(e) {
-    console.log("in list-group-item-dark.on.click");
+    console.log("in list-group-item-dark click event");
     var value = e.target.value;
     console.log("click button value: ", value);
   });
  
 
-// closes the document.ready
-}) 
+}) // closes the document.ready
